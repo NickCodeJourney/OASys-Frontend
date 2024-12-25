419565bc8132efab545412992e426b32b733faac
Nick Young <72905543+NickYoungPeng@users.noreply.github.com>
2024-12-25 13:55:26 +0800

    refactor(forms): 优化表单组件的代码结构和命名
    - 在 AddUser 和 Login 组件中，将 form 的 ref 属性改为统一的 "formRef"
    -移除了冗余的空格和换行，提高了代码的可读性
    - 在 Login 组件中，将 "forms" 参数从 onSubmit 方法中移除，改为直接使用组件数据

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,75 +1,67 @@
 <template>
-    <div class="login">
-        <el-form :model="forms" :rules="rules" :ref="forms" class="login-form">
-            <h3 class="title">
-                OA管理系统
-            </h3>
-            <el-form-item prop="username">
-                <el-input type="text" v-model="forms.username" placeholder="用户名"/>
-            </el-form-item>
-            <el-form-item prop="password">
-                <el-input type="password" v-model="forms.password" placeholder="密码"
-                          @keyup.enter.native="onSubmit(forms)"/>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="onSubmit(forms)" :loading="load" round>登录</el-button>
-            </el-form-item>
-        </el-form>
-    </div>
+  <div class="login">
+    <el-form :model="forms" :rules="rules" ref="formRef" class="login-form">
+      <h3 class="title">
+        OA管理系统
+      </h3>
+      <el-form-item prop="username">
+        <el-input type="text" v-model="forms.username" placeholder="用户名" />
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input type="password" v-model="forms.password" placeholder="密码"
+                  @keyup.enter.native="onSubmit" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit" :loading="load" round>登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
 import qs from "qs";
-import {setAuth} from "../utils/auth";
-import {getAuth, login} from "../utils/api";
+import { setAuth } from "../utils/auth";
+import { getAuth, login } from "../utils/api";
 
 export default {
-    name: "Login",
-    data() {
-        return {
-            forms: {
-                username: "",
-                password: ""
-            },
-            rules: {
-                username: [
-                    {
-                        required: true,
-                        message: "请输入用户名",
-                        trigger: "blur"
-                    }
-                ],
-                password: [
-                    {
-                        required: true,
-                        message: "请输入密码",
-                        trigger: "blur"
-                    }
-                ]
-            },
-            load: false
+  name: "Login",
+  data() {
+    return {
+      forms: {
+        username: "",
+        password: ""
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" }
+        ]
+      },
+      load: false
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$refs.formRef.validate((valid) => {
+        if (valid) {
+          this.load = true;
+          login(qs.stringify(this.forms)).then(response => {
+            this.load = false;
+            if (response && response.status === "success") {
+              getAuth().then(authResponse => {
+                setAuth(authResponse.data);
+              });
+              this.$message.success(response.message);
+              this.$router.push({ name: "index" });
+            }
+          });
         }
-    },
-    methods: {
-        onSubmit(forms) {
-            this.$refs[forms].validate((valid) => {
-                if (valid) {
-                    this.load = true
-                    login(qs.stringify(forms)).then(response => {
-                        this.load = false
-                        if (response && response.status === "success") {
-                            getAuth().then(response => {
-                                setAuth(response.data)
-                            })
-                            this.$message.success(response.message)
-                            this.$router.push({name: "index"})
-                        }
-                    })
-                }
-            })
-        }
+      });
     }
-}
+  }
+};
 </script>
 
 <style scoped>
